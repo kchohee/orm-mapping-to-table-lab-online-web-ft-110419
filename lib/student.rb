b017fd758201dced925fc86d2ef5d3b4c94bd592
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-15 04:05:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,10 +24,10 @@ class Student
   end
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
- 
+
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
