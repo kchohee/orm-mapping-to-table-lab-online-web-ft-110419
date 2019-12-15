fd65a5aa12f2f4458f4e63186e646066ce6612bc
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-15 03:55:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,14 +7,14 @@ class Student
     @id = id
   end
   def self.create_table
-    sql =  <<-SQL 
+    sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
+        id INTEGER PRIMARY KEY,
+        name TEXT,
         album TEXT
         )
         SQL
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end
 
 end
